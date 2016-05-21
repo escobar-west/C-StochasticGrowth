d41807065019763bf7906897787dd98fc940aafd
victor-suriel <victor.v.suriel@gmail.com>
2016-05-21 12:17:38 -0400

    Add files via upload

---
+++ plotter.m
@@ -0,0 +1,17 @@
+fileID = fopen('~/Documents/C++/CFDstochasticGrowth/ODEsolution.txt', 'r');
+formatSpec = '%f';
+A = fscanf(fileID, formatSpec);
+
+N = A(1);
+trials = A(2);
+
+figure
+hold on
+
+for i = 1:trials
+    plot( A(3:3 + N), A( i*(N+1)+3: i*(N+1)+3 + N ))
+end
+
+hold off
+
+fclose(fileID)
